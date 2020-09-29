03e6beb4afe4828a949722b639b85a6b0f34e0b7
giorgi <gio-oig>
2020-09-29 20:49:26 +0400

    Updates

---
+++ precache-manifest.f8a1e6bfbc634bf38da9422a6bd02735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5156aae6be6148c387b88a4b41040e",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "035808bf79e7edb47bea",
+    "url": "/react-quiz-app/static/css/main.c5aa658c.chunk.css"
+  },
+  {
+    "revision": "dfb149c98b6fe9b640f5",
+    "url": "/react-quiz-app/static/js/2.0a2e87d2.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-quiz-app/static/js/2.0a2e87d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035808bf79e7edb47bea",
+    "url": "/react-quiz-app/static/js/main.50adf5c2.chunk.js"
+  },
+  {
+    "revision": "4cb17b25e4d85c130919",
+    "url": "/react-quiz-app/static/js/runtime-main.0678ee4d.js"
+  }
+]);
